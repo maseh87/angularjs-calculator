14143af8684c114433e7687f382d0cb3c7ffbd65
Mason <maseh87@gmail.com>
2014-04-28 15:44:03 -0700

    added calculator buttons and output message

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 angular.module('myApp', [])
 
+
+
+
 .controller('myController', function($scope) {
-  $scope.name = 'Mason';
+  var loggedNums = 0;
+  $scope.output = 0;
+  $scope.numbers = function(btn) {
+    if($scope.output === 0) {
+      $scope.output = btn.toString();
+    } else {
+      $scope.output += btn.toString();
+    }
+  }
+  $scope.clear = function() {
+    $scope.output = 0;
+  }
+  $scope.add = function() {
+    loggedNums = parseInt($scope.output);
+    $scope.output += '+';
+  }
 });
